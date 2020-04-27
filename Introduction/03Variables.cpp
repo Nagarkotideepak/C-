b710b0ad20f9118317b31f7cca2ba21499d0b04a
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-04-27 11:25:10 +0545

    Create 03Variables.cpp

---
+++ Introduction/03Variables.cpp
@@ -0,0 +1,11 @@
+// Variable" name given to the value
+// Syntax: data_type vairable_name = value;
+#include<iostream>
+using namespace std;
+int main(){
+
+	int firstnumber=10;
+	int secondnumber =20;
+	int sum = firstnumber+secondnumber;
+	cout<<"The Sum is "<<sum;	
+}
