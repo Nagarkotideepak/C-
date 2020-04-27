490c92dddc8dcaa90a112f547ebf72374cc481a9
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-04-27 11:18:53 +0545

    Create 02DataTypes.cpp

---
+++ Introduction/02DataTypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Initialize variables
+	int a=10.1;
+	float b= 20.5;
+	double c=30.5;
+	bool d=true;
+	string name="Deep";
+  // Displaying
+	cout<<"The value of a is "<<a<<endl;
+  cout<<"The value of b is "<<b<<endl;
+	cout<<"The value of c is "<<c<<endl;
+  cout<<"The value of d is "<<d<<endl; //value is 1 when true and 0 when false
+	cout<<"The value of name is "<<name<<endl;
+	return 0;
+	
+}
