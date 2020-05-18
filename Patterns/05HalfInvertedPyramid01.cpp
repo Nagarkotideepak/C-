ceba1cdbba742b4f473ee9290abd15da934c31ec
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-18 10:55:26 +0545

    Update 05HalfInvertedPyramid01.cpp

--- Patterns/05HalfInvertedPyramid01.cpp
+++ Patterns/05HalfInvertedPyramid01.cpp
@@ -16,3 +16,25 @@ int main(){
 	
 	return 0;
 }
+
+//OR
+/*
+****
+***
+**
+*
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int count=1;
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			if(i<=j)
+				cout<<"* ";
+		}
+	cout<<endl;
+	}
+	return 0;
+}
+*/
