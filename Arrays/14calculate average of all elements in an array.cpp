549174e2949fe1390ff22e62ff2c2b53eef2b778
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-20 21:08:26 +0545

    Create 14calculate average of all elements in an array.cpp
    calculate average of all elements in an array

---
+++ Arrays/14calculate average of all elements in an array.cpp
@@ -0,0 +1,18 @@
+//Wap to calculate average of all elements in an array
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i;
+	float num[100],sum=0.0,average;
+	cout<<"Enter the numbers of element: ";
+	cin>>n;
+	for(i=0;i<n;++i){
+		cout<<i+1<<". Enter Number: ";
+		cin>>num[i];
+		sum += num[i];
+	}
+	average =sum/n;
+	cout<<"Average = "<<average;
+
+	return 0;
+}
