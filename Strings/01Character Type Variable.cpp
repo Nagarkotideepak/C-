a8aa3198402485005175e77415971c6394acc515
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-27 18:45:40 +0545

    Create 01Character Type Variable.cpp
    Character Type Variable

---
+++ Strings/01Character Type Variable.cpp
@@ -0,0 +1,16 @@
+//Character Type Variable
+#include<iostream>
+using namespace std;
+int main(){
+	//Decalaration and Initialization of Character type Variable
+	char temp; // it takes 1 byte of memmory
+	temp='A';
+	/*
+	Wrong Syntax
+	temp='AB';
+	temp=A;
+	temp="A";
+	*/
+	cout<<temp;
+	return 0;
+}
