2ceb56a67c177fc4655176008de06f4c3a33a2f7
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-27 18:46:52 +0545

    Create 03String.cpp
    String

---
+++ Strings/03String.cpp
@@ -0,0 +1,15 @@
+//String
+#include<iostream>
+using namespace std;
+int main(){
+	char A[10]={'D','E','E','P','A','K','\0'};
+	// [D,E,E,P,A,K,0,0,0,0]
+	//String is a set of character
+	/*
+	char A[]="Deep":
+	char *name="Deep"; // stored in Heap
+	
+	*/
+	cout<<A;
+	return 0;
+}
