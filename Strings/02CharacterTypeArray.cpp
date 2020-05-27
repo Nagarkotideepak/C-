f87d56e2a31c0fca6ba76f8801ca580be5545094
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-27 18:46:14 +0545

    Create 02CharacterTypeArray.cpp
    CharacterType variable in Array

---
+++ Strings/02CharacterTypeArray.cpp
@@ -0,0 +1,17 @@
+//CharacterTypeArray
+#include<iostream>
+using namespace std;
+int main(){
+	// Initialization and Declaration of Array of character
+	char A[5];
+	A[5]={'A','B','C','D','E'};
+	/* 
+	OR
+	A[]={'A','B','C','D','E'};
+	A[]={65,66,67,68,69};
+	
+	*/
+	char B[5]={'A','B'};// it will store [A,B,0,0,0]
+	
+	return 0;
+}
