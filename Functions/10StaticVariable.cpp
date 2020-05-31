18c37ebdfb04fd2d91c217d6af5f048f8cda46c7
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-31 10:37:20 +0545

    Create 10StaticVariable.cpp
    Static Variable

---
+++ Functions/10StaticVariable.cpp
@@ -0,0 +1,15 @@
+//Static Variable
+#include<iostream>
+using namespace std;
+void fun(){
+	static int v=0; //Static Variable
+	int a=5;
+	v++;
+	cout<<a<<" "<<v<<endl;
+}
+int main(){
+	fun();
+	fun();
+	fun();
+	return 0;
+}
